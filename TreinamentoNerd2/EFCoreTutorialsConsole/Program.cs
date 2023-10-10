8e74a80623eaf8ab442d21bc199b5b99bac485d0
Marcelo Duarte <marcelo.duarte@nerd2.com.br>
2023-10-10 20:37:23 -0300

    Operações de CRUD com EF Core 7

--- TreinamentoNerd2/EFCoreTutorialsConsole/Program.cs
+++ TreinamentoNerd2/EFCoreTutorialsConsole/Program.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Net.Http.Headers;
 
 namespace EFCoreTutorialsConsole
 {
@@ -6,7 +8,28 @@ namespace EFCoreTutorialsConsole
     {
         static void Main(string[] args)
         {
-            CriarEntidade();
+            Console.WriteLine("Entre com o primeiro nome do estudante:");
+            var nomeEstudante = Console.ReadLine();
+
+            Console.WriteLine("Entre com o sobrenome do estudante:");
+            var sobrenomeEstudante = Console.ReadLine();
+
+            CriarEntidade(nomeEstudante, sobrenomeEstudante);
+
+            var std = new Student { FirstName = nomeEstudante, LastName = sobrenomeEstudante + "-Detached" };
+
+            CriarEntidadeDesatachadaDoContexto(std);
+
+
+            ModificarEntidade(nomeEstudante, sobrenomeEstudante);
+
+            ExcluirEntidade();
+
+            GetPeloNome(nomeEstudante);
+
+            GetPeloNomeComInclude(nomeEstudante);
+
+
 
             ListarEstadoUnchangedEntidades();
 
@@ -63,16 +86,16 @@ namespace EFCoreTutorialsConsole
         }
 
 
-        private static void CriarEntidade()
+        private static void CriarEntidade(string nome, string sobrenome)
         {
             using (SchoolContext context = new SchoolContext())
             {
                 //Criar BD se não existir
-                context.Database.EnsureCreated();
+                //context.Database.EnsureCreated();
 
                 //Criar uma entidade de objeto
-                var grade01 = new Grade() { GradeName = "Curso C# .Net" };
-                var student01 = new Student() { FirstName = "Joaninha", LastName = "Silveirinha", Grade = grade01 };
+                Grade grade01 = new Grade() { GradeName = "Curso C# .Net" };
+                Student student01 = new Student() { FirstName = nome, LastName = sobrenome, Grade = grade01 };
 
                 //Adicionar a entidade ao contexto
                 context.Students.Add(student01);
@@ -80,10 +103,9 @@ namespace EFCoreTutorialsConsole
                 //Salvar a entidade estudante
                 context.SaveChanges();
 
-                foreach (var std in context.Students)
+                foreach (Student std in context.Students)
                 {
                     Console.WriteLine(std.FirstName + " " + std.LastName);
-
                 }
 
             }
@@ -107,5 +129,92 @@ namespace EFCoreTutorialsConsole
                     $"State: {entry.State.ToString()}");
             }
         }
+
+
+        private static void ModificarEntidade(string novoNome, string novoSobrenome)
+        {
+            try
+            {
+
+                using (SchoolContext ctx = new SchoolContext())
+                {
+
+                    var std = ctx.Students.First<Student>();
+                    std.FirstName = novoNome;
+                    std.LastName = novoSobrenome;
+
+                    ctx.SaveChanges();
+                }
+
+            }
+            catch (DbUpdateConcurrencyException ex01)
+            {
+                //throw new Exception("Record does not exist in the database");
+                Console.WriteLine($"Registro nao existe no banco de dados {ex01.Message}");
+
+            }
+            catch (Exception ex02)
+            {
+                Console.WriteLine($"Ocorreu a seguinte exceção: {ex02.Message}");
+            }
+
+
+        }
+
+        private static void ExcluirEntidade()
+        {
+            using (SchoolContext schoolContext = new SchoolContext())
+            {
+                var std = schoolContext.Students.First<Student>();
+                schoolContext.Students.Remove(std);
+                // ou
+                // schoolContext.Remove<Student>(std);
+
+                schoolContext.SaveChanges();
+
+            }
+        }
+
+        private static void GetPeloNome(string nome)
+        {
+            var context = new SchoolContext();
+            List<Student> studentsWithSameName = context.Students.Where(std => std.FirstName == nome).ToList();
+
+            Console.WriteLine("Listando Estudante...");
+            foreach (var item in studentsWithSameName)
+            {
+                Console.WriteLine(item.FirstName);
+            }
+        }
+
+
+        private static void GetPeloNomeComInclude(string nome)
+        {
+            var context = new SchoolContext();
+
+            var studentWithGrade = context.Students
+                                       .Where(s => s.FirstName == nome)
+                                       .Include(s => s.Grade)
+                                       .FirstOrDefault();
+
+            Console.WriteLine("...Listando Estudante com Grade usando Include... " +
+               studentWithGrade.FirstName + " - " + studentWithGrade.Grade.GradeName);
+           
+        }
+
+        private static void CriarEntidadeDesatachadaDoContexto(Student std)
+        {
+            std.Grade = new Grade { GradeName = "Outra Materia" };
+            
+            using (var ctx = new SchoolContext())
+            {
+                ctx.Add<Student>(std);
+
+                ctx.SaveChanges();
+
+            }
+
+        }
+
     }
 }
